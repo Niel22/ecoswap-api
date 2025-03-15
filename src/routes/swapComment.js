a5eb48f866d236f9a83cc0253470fd1488bf435e
Niel22 <niel2264@gmail.com>
2025-03-15 06:14:47 +0100

    fixing swap post comment

--- src/routes/swapComment.js
+++ src/routes/swapComment.js
@@ -4,6 +4,6 @@ const { create } = require('../controllers/SwapCommentController');
 const SwapCommentRequest = require('../request/SwapCommentRequest');
 const router = express.Router();
 
-router.post('/', upload('comments').array(5), SwapCommentRequest, create);
+router.post('/', upload('comments').array("images", 5), SwapCommentRequest, create);
 
 module.exports = router;
